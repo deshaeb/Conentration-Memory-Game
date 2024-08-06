0acbd8f0da2845b781b2cf8a9ee5cd5b2ecb5d68
User Name <boss@DeShaes-Mini.lan>
2024-08-06 14:48:08 -0400

    images added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
   /*----- constants -----*/
-
+const frontTile = [
+  {name: 'HartPlaza', img: 'https://imgur.com/a/U0cpQxs', matched: false},
+  {name: 'Riverwalk', img: 'https://imgur.com/94ZnTIq', matched: false},
+  {name: 'Campus Martius', img: 'https://imgur.com/3CBSrIG', matched: false},
+  {name: 'Comerica Park', img: 'https://imgur.com/JELf4Mo', matched: false},
+  {name: 'LCA', img: 'https://imgur.com/XbLCDgm', matched: false},
+  {name: 'Ford Field', img: 'https://imgur.com/E4t6vfV', matched: false},
+  {name: 'Casino Life', img: 'https://imgur.com/GVQjbwe', matched: false},
+  {name: 'Rennaisance', img: 'https://imgur.com/wJsEKRp', matched: false},
+];
+const backTile = 'https://imgur.com/a/VzY6lKF';
 
   /*----- state variables -----*/
-
+let board;
+let cardSelected;
+let winner;
+let loser;
 
   /*----- cached elements  -----*/
 
