fa09d423f506217a4391c1c0c2eaac5ac406015a
User Name <boss@DeShaes-Mini.lan>
2024-08-07 15:06:03 -0400

    render function

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
   /*----- constants -----*/
 const frontTileSource = [
-  {name: 'HartPlaza', img: '../concentrationImg/hartPlaza.jpg', matched: false},
-  {name: 'Riverwalk', img: '../concentrationImg/riverWalk.jpg', matched: false},
-  {name: 'Campus Martius', img: '../concentrationImg/campusMartius.jpg', matched: false},
-  {name: 'Comerica Park', img: '../concentrationImg/comericaPark.jpg', matched: false},
-  {name: 'LCA', img: '../concentrationImg/lca.jpg', matched: false},
-  {name: 'Ford Field', img: '../concentrationImg/fordField.jpg', matched: false},
-  {name: 'Casino Life', img: '../concentrationImg/motorCity.jpg', matched: false},
-  {name: 'Rennaisance', img: '../concentrationImg/rennBuilding.jpg', matched: false},
+  {img: '../concentrationImg/hartPlaza.jpg', matched: false},
+  {img: '../concentrationImg/riverWalk.jpg', matched: false},
+  {img: '../concentrationImg/campusMartius.jpg', matched: false},
+  {img: '../concentrationImg/comericaPark.jpg', matched: false},
+  {img: '../concentrationImg/lca.jpg', matched: false},
+  {img: '../concentrationImg/fordField.jpg', matched: false},
+  {img: '../concentrationImg/motorCity.jpg', matched: false},
+  {img: '../concentrationImg/rennBuilding.jpg', matched: false},
 ];
 const backTile = '../concentrationImg/tileBack.jpg';
 
   /*----- state variables -----*/
-let board; //Array of 16 shuffled card objects
-let tileSelected;
+let tiles; //Array of 16 shuffled card objects
+let tileSelected; //first tile clicked (tile object) or null
 //let tiles;
 //let firstTile;
 let winner;
@@ -26,25 +26,33 @@ const tileImgEl = document.querySelectorAll('main > img');
 
 
   /*----- functions -----*/
-  init (); //initialize all state then call render()
-
+  init (); 
+//initialize all state then call render()
   function init() {
-    board = getShuffledTiles();
-    console.log(board);
+    tiles = getShuffledTiles();
+    tileSelected = null;
+    render();
   }
   
+  function render() {
+    tiles.forEach(function(tile, idx) {
+      const imgEl = document.getElementById(idx);
+      const src = (tile.matchd || tile === tileSelected) ? tile.img : backTile;
+      imgEl.src = src;
+    });
+  }
+
   function getShuffledTiles() {
-    const tempTiles = [];
-    const tiles = []
+    let tempTiles = [];
+    let tiles = []
     for (let tile of frontTileSource) {
-      tempTiles.push(tile, tile);
+      tempTiles.push({...tile}, {...tile});
     }
     while (tempTiles.length) {
       let rndIdx = Math.floor(Math.random() * tempTiles.length);
       let tile = tempTiles.splice(rndIdx, 1)[0];
-    
-    console.log(tile);
+      tiles.push(tile);
   }
 
-  return tiles;
+return tiles;
   }
