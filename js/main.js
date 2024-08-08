b0f0c04b3ee21c2570a01d86bdd6ccb51964d46f
User Name <boss@DeShaes-Mini.lan>
2024-08-08 08:44:18 -0400

    event target 1

--- js/main.js
+++ js/main.js
@@ -14,16 +14,14 @@ const backTile = '../concentrationImg/tileBack.jpg';
   /*----- state variables -----*/
 let tiles; //Array of 16 shuffled card objects
 let tileSelected; //first tile clicked (tile object) or null
-//let tiles;
-//let firstTile;
 let winner;
 let loser;
 
   /*----- cached elements  -----*/
-const tileImgEl = document.querySelectorAll('main > img');
+// const tileImgEl = document.querySelectorAll('section > img');
 
   /*----- event listeners -----*/
-
+  document.querySelector('#board').addEventListener('click', handleChoice);
 
   /*----- functions -----*/
   init (); 
@@ -38,6 +36,7 @@ const tileImgEl = document.querySelectorAll('main > img');
     tiles.forEach(function(tile, idx) {
       const imgEl = document.getElementById(idx);
       const src = (tile.matchd || tile === tileSelected) ? tile.img : backTile;
+      console.log(imgEl)
       imgEl.src = src;
     });
   }
@@ -55,4 +54,9 @@ const tileImgEl = document.querySelectorAll('main > img');
   }
 
 return tiles;
+  }
+
+  function handleChoice(event) {
+    const tileIdx = parseInt(event.targer.id);
+    console.log(cardIdx)
   }
