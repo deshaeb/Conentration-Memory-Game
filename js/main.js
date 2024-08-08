89a2bfebfe6a7f588f7487c46ec4846642f2e96c
User Name <boss@DeShaes-Mini.lan>
2024-08-08 19:51:33 -0400

    MVP Met

--- js/main.js
+++ js/main.js
@@ -18,17 +18,20 @@ let tileSelected2;
 let ignoreClicks;
 let wrongTiles;
 let matchesMade;
-let count = 5;
 let timer;
 let winner;
 let loser;
 
 /*----- cached elements  -----*/
-const msgEl = document.querySelector('#wrongGuess');
-///const displayTmr = document.querySelector('#timer');
+const msgGuess = document.querySelector('#wrongGuess');
+const msgMatch = document.querySelector('#matchesMade');
+const msgEl = document.querySelector('#displayMsg');
+const msgInstr = document.querySelector('#displayMsg');
 
 /*----- event listeners -----*/
 document.querySelector('#board').addEventListener('click', handleChoice);
+document.querySelector('#play').addEventListener('click',init);
+document.querySelector('#shuffle').addEventListener('click',init);
 
 /*----- functions -----*/
 init();
@@ -38,24 +41,29 @@ function init() {
   tileSelected1 = null;
   tileSelected2 = null;
   wrongTiles = 0;
+  matchesMade = 0;
   ignoreClicks = false;
-  //count = 5;
-  //timer = setInterval(handleChoice, 500);
-
+  winner = null;
+  loser = null;
+  msgInstr.innerHTML = `INSTRUCTIONS<br>
+        Click 'Play!' to begin. <br>
+        Select 2 tiles per turn! <br>
+        Collect 8 matches to win! <br>
+        8 wrong picks = GAME OVER! <br>
+        Click 'Shuffle' to restart the game. <br>
+        Good Luck!`;
   render();
 }
 
 function render() {
-  // if (tileSelected1) {
-  //   timer = setInterval(startTimer, 1000)
-  // }
   tiles.forEach(function (tile, idx) {
     const imgEl = document.getElementById(idx);
     const src = (tile.matched || tile === tileSelected1 || tile === tileSelected2) ? tile.img : backTile;
-    //console.log(imgEl)
     imgEl.src = src;
   });
-  msgEl.innerHTML = `Wrong: ${wrongTiles}`;
+  msgGuess.innerHTML = `Wrong: ${wrongTiles}/8`;
+  msgMatch.innerHTML = `Matches: ${matchesMade}/8`;
+  checkWinner()
 }
 
 function getShuffledTiles() {
@@ -79,57 +87,42 @@ function handleChoice(event) {
   if (isNaN(tileIdx) || ignoreClicks) return;
   if (tileSelected1 && tileSelected2) return;
   const tile = tiles[tileIdx]
-  //clearInterval(timer)
   console.log(tileSelected1)
   console.log(tileSelected2)
   if (tileSelected1 && !tileSelected2) {
-    //console.log('tile 1 true');
-    tileSelected2 = tile
+    tileSelected2 = tile;
     render()
     if (tileSelected1.img === tileSelected2.img) {
       tileSelected1.matched = tileSelected2.matched = true;
+      matchesMade++;
       tileSelected1 = null;
       tileSelected2 = null;
-
+      render()
   } else {
     tileSelected1 = null;
     tileSelected2 = null;
     wrongTiles++;
+    msgEl.innerHTML = `Wrong! Try Again!`
     setTimeout(() => {
       render()
     }, 2000)}
   } else {
     tileSelected1 = tile
+    msgEl.innerHTML = ``;
     render()
   }
 }
 
-// function checkWinner() {
-//   if wrong
-//if wrong guess === 8 , set loser = true;
-//if matches ===8, set winner = true
-// };
-
-// function startTimer() {
-//   count--;
-//   console.log(count)
-//   if (count === 0) {
-//     //wrongTiles++
-//     count = 5;
-//     //render()
-//     clearInterval(timer)
-//     tileSelected1 = null
-// handleChoice(event)
-//   } 
-// };
-
-function startTimer(callback, minutes, seconds) {
-  const interval = (minutes * 60 + seconds) * 1000; // Convert to milliseconds
-  return setInterval(callback, interval);
-}
-startTimer(() => {
-
-}, 3, 59)
-
-
-// if (isNaN(tileIdx) || winner || )
+function checkWinner() {
+  if (matchesMade === 8) {
+    winner = true;
+    msgEl.innerHTML = `WINNER!`;
+    ignoreClicks = true;
+  } else if (wrongTiles === 8) {
+    loser = true;
+    msgEl.innerHTML = `YOU LOSE!`;
+    ignoreClicks = true;
+  } else {
+    return;
+  }
+};
